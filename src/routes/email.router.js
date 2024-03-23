35d600a66f0ef79546c9deb49951960de918465b
Santiago Garzon <santigg90@gmail.com>
2024-03-23 12:40:37 -0500

    email 004

--- src/routes/email.router.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const emailController = require("../controllers/email.controller.js");
-
-const router = Router();
-
-router.post("/contact", emailController.sendContactForm);
-
-module.exports = router;
