3be38c0521a9fc933734e5047a9b9f4df6ef0634
Santiago Garzon <santigg90@gmail.com>
2024-03-24 23:15:51 -0500

    0021

--- src/uploader.image.js
+++ src/uploader.image.js
@@ -6,30 +6,28 @@ const { ERROR_UPLOAD_IMAGE } = require("./constants/messages.js");
 const { getRandomNumber } = require("./helpers/math.helper.js");
 const { getDatetimeAsInteger } = require("./helpers/datetime.helper.js");
 
-const configureMulter = () => {
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, DIR_IMAGES_PATH);
-        },
-        filename: function (req, file, cb) {
-            const datetime = getDatetimeAsInteger();
-            const randomNumber = getRandomNumber(100000, 999999);
-            const extension = file.originalname.slice(file.originalname.lastIndexOf("."));
-            const filename = `img_${randomNumber}_${datetime}${extension}`;
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, DIR_IMAGES_PATH);
+    },
+    filename: function (req, file, cb) {
+        const datetime = getDatetimeAsInteger();
+        const randomNumber = getRandomNumber(100000, 999999);
+        const extension = file.originalname.slice(file.originalname.lastIndexOf("."));
+        const filename = `img_${randomNumber}_${datetime}${extension}`;
 
-            cb(null, filename);
-        },
-    });
+        cb(null, filename);
+    },
+});
 
-    const fileFilter = function (req, file, cb) {
-        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-            cb(null, true);
-        } else {
-            cb(new multer.MulterError(400, ERROR_UPLOAD_IMAGE), false);
-        }
-    };
-
-    return multer({ storage, fileFilter });
+const fileFilter = function (req, file, cb) {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(new multer.MulterError(400, ERROR_UPLOAD_IMAGE), false);
+    }
 };
 
-module.exports = configureMulter;
+const uploaderImage = multer({ storage, fileFilter });
+
+module.exports = uploaderImage;
