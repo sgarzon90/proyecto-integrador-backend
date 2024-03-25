cac097a8a41186c2b32b202441ea7cb2f83138d5
Santiago Garzon <santigg90@gmail.com>
2024-03-24 23:06:16 -0500

    rb

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,12 +1,15 @@
 const path = require("path");
 const { getCollection, generateId } = require("../connectionDB.js");
 const { HEADER_CONTENT_TYPE } = require("../constants/headers.js");
+
 const {
     ERROR_ID_NOT_FOUND,
     ERROR_SERVER,
+    ERROR_UPLOAD_NULL,
 } = require("../constants/messages.js");
 const { DIR_IMAGES_PATH } = require("../constants/paths.js");
 const { deletefile } = require("../fileSystem.js");
+
 const normalizeValue = (value) => {
     return value
         .toUpperCase()
@@ -17,11 +20,13 @@ const normalizeValue = (value) => {
         .replace("Ó", "O")
         .replace("Ú", "U");
 };
+
 const createSchema = (values) => {
     const {
         id,
         name,
         description,
+        imageFileName,
         stock,
         price,
         isPromotion,
@@ -31,10 +36,12 @@ const createSchema = (values) => {
         isNational,
         freeShipping,
     } = values;
+
     return {
         id: Number(id),
         name: normalizeValue(name),
         description: description ?? null,
+        imageFileName,
         stock: Number(stock),
         price: Number(price),
         isPromotion: Boolean(isPromotion),
@@ -45,49 +52,70 @@ const createSchema = (values) => {
         freeShipping: Boolean(freeShipping),
     };
 };
+
 const deleteImage = (imageFileName) => {
     if (imageFileName && imageFileName.length > 0) {
         const filePath = path.join(DIR_IMAGES_PATH, imageFileName);
+
         if (imageFileName != "default.jpg") {
             deletefile(filePath);
         }
     }
 };
+
 const getAll = async (req, res) => {
     res.set(HEADER_CONTENT_TYPE);
+
     try {
         const { search } = req.query;
         const filters = {};
+
         if (search) {
             filters["$or"] = [{ id: Number(search) }, { name: { $regex: normalizeValue(search), $options: "i" } }];
         }
+
         const collection = await getCollection("products");
         const products = await collection.find(filters).sort({ name: 1 }).hint("idx_id").hint("idx_name").toArray();
+
         res.status(200).send({ success: true, data: products });
     } catch (error) {
         console.log(error);
         res.status(500).send({ success: false, message: ERROR_SERVER });
     }
 };
+
 const getOne = async (req, res) => {
     res.set(HEADER_CONTENT_TYPE);
+
     try {
         const { id } = req.params;
+
         const collection = await getCollection("products");
         const product = await collection.findOne({ id: Number(id) });
+
         if (!product) return res.status(404).send({ success: false, message: ERROR_ID_NOT_FOUND });
-        res.status(200).send({ success: true, data: product });
+
+        res.status(200).send({ success: false, data: product });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ success: false, message: ERROR_SERVER });
     }
 };
+
 const create = async (req, res) => {
     res.set(HEADER_CONTENT_TYPE);
+
     try {
         const collection = await getCollection("products");
         const id = await generateId(collection);
-        const productData = createSchema({ ...req.body, id });
+
+        // Verificamos si se ha enviado una imagen
+        let imageFileName = "https://puntooriente.onrender.com/public/images/img_546820_20240323232547.jpg"; // Imagen predeterminada en caso de que no se envíe ninguna
+        if (req.file) {
+            imageFileName = req.file.filename; // Nombre de archivo de la imagen subida
+        }
+
+        const productData = createSchema({ ...req.body, id, imageFileName });
         await collection.insertOne(productData);
         res.status(201).send({ success: true, data: productData });
     } catch (error) {
@@ -95,35 +123,73 @@ const create = async (req, res) => {
         res.status(500).send({ success: false, message: ERROR_SERVER });
     }
 };
+
 const update = async (req, res) => {
     res.set(HEADER_CONTENT_TYPE);
+
     try {
         const { id } = req.params;
+
         const collection = await getCollection("products");
         const product = await collection.findOne({ id: Number(id) });
+
         if (!product) return res.status(404).send({ success: false, message: ERROR_ID_NOT_FOUND });
-        // Eliminar el campo "id" de req.body antes de actualizar
-        const { id: productId, ...values } = req.body;
-        const updatedValues = createSchema({ id: Number(id), ...values });
-        await collection.updateOne({ id: Number(id) }, { $set: updatedValues });
-        res.status(200).send({ success: true, data: updatedValues });
+
+        // Verificamos si se ha enviado una imagen
+        let imageFileName = product.imageFileName; // Mantenemos la imagen existente por defecto
+        if (req.file) {
+            imageFileName = req.file.filename; // Nombre de archivo de la nueva imagen subida
+        }
+
+        const values = createSchema({ id, ...req.body, imageFileName });
+        await collection.updateOne({ id: Number(id) }, { $set: values });
+
+        if (product.imageFileName != values.imageFileName) {
+            deleteImage(product.imageFileName);
+        }
+
+        res.status(200).send({ success: true, data: values });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ success: false, message: ERROR_SERVER });
     }
 };
+
 const remove = async (req, res) => {
     res.set(HEADER_CONTENT_TYPE);
+
     try {
         const { id } = req.params;
+
         const collection = await getCollection("products");
         const product = await collection.findOne({ id: Number(id) });
+
         if (!product) return res.status(404).send({ success: false, message: ERROR_ID_NOT_FOUND });
+
         await collection.deleteOne({ id: Number(id) });
+
+        deleteImage(product.imageFileName);
+
         res.status(200).send({ success: true, data: product });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ success: false, message: ERROR_SERVER });
     }
 };
-module.exports = { getAll, getOne, create, update, remove };
+
+const uploadImage = async (req, res) => {
+    res.set(HEADER_CONTENT_TYPE);
+
+    try {
+        const file = req.file;
+
+        if (!file) return res.status(400).send({ success: false, message: ERROR_UPLOAD_NULL });
+
+        res.status(201).send({ success: true, data: file });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ success: false, message: ERROR_SERVER });
+    }
+};
+
+module.exports = { getAll, getOne, create, update, remove, uploadImage };
