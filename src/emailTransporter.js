80ce90cd3b538f42b2c07b93a6de8ef80803207e
Santiago Garzon <santigg90@gmail.com>
2024-03-24 22:54:08 -0500

    005

--- src/emailTransporter.js
+++ src/emailTransporter.js
@@ -1,13 +1,12 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config({ path: ENV_PATH });
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    host: "smtp-relay.brevo.com",
+    port: 587,
     secure: false, // true para usar SSL/TLS
     auth: {
-        user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_PASSWORD,
+        user: "santigg90@gmail.com",
+        pass: "APUQr0Ss8ZM1Xfzn",
     },
 });
 
