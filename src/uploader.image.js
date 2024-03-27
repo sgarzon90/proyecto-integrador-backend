2441f4011451da856ffdfe8b2963fd493fb34ade
Santiago Garzon <santigg90@gmail.com>
2024-03-27 11:07:16 -0500

    crud 006

--- src/uploader.image.js
+++ src/uploader.image.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = function (req, file, cb) {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
         cb(null, true);
     } else {
         cb(new multer.MulterError(400, ERROR_UPLOAD_IMAGE), false);
