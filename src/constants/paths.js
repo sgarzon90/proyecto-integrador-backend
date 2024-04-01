1636f03a5f824db45d6cd061fba84b1af75a347c
Santiago Garzon <santigg90@gmail.com>
2024-04-01 13:18:44 -0500

    img 007

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const ENV_PATH = path.join(__dirname, "../../.env");
 const DIR_PUBLIC_PATH = path.join(__dirname, "../public");
-const DIR_IMAGES_PATH = path.join("../public/images");
+const DIR_IMAGES_PATH = "../public/images";
 
 module.exports = {
     ENV_PATH,
