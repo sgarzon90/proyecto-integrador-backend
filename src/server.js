b3c78975988bb6d2a3ada0b0dabc332695aaf1cd
Santiago Garzon <santigg90@gmail.com>
2024-04-01 14:05:23 -0500

    img 11

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { sendEmail } = require("./controllers/contact.controller.js");
 const productsRouter = require("./routes/products.router.js");
 const database = require("./connectionDB.js");
 
-const { ENV_PATH, DIR_PUBLIC_PATH } = require("./constants/paths.js");
+const { ENV_PATH, DIR_PUBLIC_PATH, DIR_IMAGES_PATH } = require("./constants/paths.js");
 const { ERROR_SERVER } = require("./constants/messages.js");
 
 // variables de entorno
@@ -24,8 +24,11 @@ server.use(cors());
 server.options("", cors());
 server.use("/api/products", productsRouter);
 
-// Configuración de carpeta estática
+// Configurar la ruta para servir imágenes estáticas
 server.use("/images", express.static(DIR_IMAGES_PATH));
+
+// Agregar configuración para servir otros archivos estáticos (si es necesario)
+server.use(express.static(DIR_PUBLIC_PATH));
 
 // Control de errores
 server.use((error, req, res, next) => {
